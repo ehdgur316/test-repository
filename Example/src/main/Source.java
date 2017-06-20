a35e71155095db62490a270680f1287e44d22c21
j <j@j-PC>
2017-06-20 14:34:30 +0900

    While Loop Added

--- Example/src/main/Source.java
+++ Example/src/main/Source.java
@@ -1,18 +1,24 @@
 package main;
+
 /*Github Branch Test*/
 import java.util.Scanner;
 
 public class Source {
 	public static void main(String[] args) {
-		
-		System.out.print("Input Text : ");
-		Scanner sc = new Scanner(System.in);
+		int trigger = 0;
 
-		String str = sc.nextLine();
-		if (str.equalsIgnoreCase("Alert")) System.out.println("Cancelled");
-		else System.out.println("Repeat Text : " + str);
+		while (trigger == 0) {
+			System.out.print("Input Text : ");
+			Scanner sc = new Scanner(System.in);
+
+			String str = sc.nextLine();
+			if (str.equalsIgnoreCase("Alert")) {
+				System.out.println("Cancelled");
+				trigger = 1;
+				sc.close();
+			} else
+				System.out.println("Repeat Text : " + str);
+		}
 		System.out.println("END");
-		
-		sc.close();
 	}
 }
